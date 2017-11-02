ebb7dd551e476b46436ff838681e928a61bd8958
Iris Schlundt Bodien <irisschlundtbodien@wcw-stud-145-109-2-182.wireless.uva.nl>
2017-11-02 15:53:25 +0100

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Iris Schlundt Bodien on 02-11-17.
+//  Copyright © 2017 Iris Schlundt Bodien. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var lightOn = true
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func buttonPressed(_ sender: Any) {
+        lightOn = !lightOn
+        updateUI()
+        
+    }
+    
+    func updateUI() {
+        view.backgroundColor = lightOn ? .white : .black
+    }
+    
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
